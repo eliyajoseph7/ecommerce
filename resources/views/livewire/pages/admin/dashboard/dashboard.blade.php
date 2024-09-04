b9eb8d3486c464c5798dc48f6faf49009b1f27ad
eliyajoseph7 <exaveryeliya20@gmail.com>
2024-09-04 16:33:20 +0300

    modified

--- resources/views/livewire/pages/admin/dashboard/dashboard.blade.php
+++ resources/views/livewire/pages/admin/dashboard/dashboard.blade.php
@@ -1,48 +1,23 @@
 <div class="mt-4 md:mt-20">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
         <div class="">
-            <div class="bg-gray-50 rounded-md h-20">
-                <div class="">Total sales</div>
-                <div class="">lor</div>
-            </div>
-            <div class="bg-gray-50 mt-2">
-                <div class="">Best selling products</div>
-                <div class="h-60 overfolw-x-auto">
-                    <table class="w-full overflow-x-auto">
-                        <tr>
-                            <td class="">1.</td>
-                            <td class="">Kiatu</td>
-                            <td class="">90%</td>
-                        </tr>
-                        <tr>
-                            <td class="">1.</td>
-                            <td class="">Kiatu</td>
-                            <td class="">90%</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+            @livewire('pages.admin.dashboard.components.stats.total-sales')
+            @livewire('pages.admin.dashboard.components.best-selling-items')
         </div>
         <div class="">
-            <div class="bg-gray-50 h-20">
-                <div class="">System visits</div>
-            </div>
+            @livewire('pages.admin.dashboard.components.stats.system-visits')
+            @livewire('pages.admin.dashboard.components.order-summary')
+            
             <div class="bg-gray-50 mt-2">
-                <div class="">Orders summary</div>
-                <div class="h-60"></div>
+                <div class="">New vs. Returning Customers</div>
             </div>
         </div>
-        <div class="bg-gray-50">
-            <div class="">Most viewed items</div>
-        </div>
+        @livewire('pages.admin.dashboard.components.item-view-trend')
         <div class="bg-gray-50">
             <div class="">Best selling categories</div>
         </div>
         <div class="bg-gray-50">
             <div class="">Discounted items</div>
-        </div>
-        <div class="bg-gray-50">
-            <div class="">New vs. Returning Customers</div>
         </div>
     </div>
 </div>
